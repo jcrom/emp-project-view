419ae85a5b13a0038d4c24793b4c531e0ac32640
Ju.chao <cqjcrom@gmail.com>
2016-11-07 15:29:17 +0800

    initial plugin.

---
+++ spec/emp-project-view-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import EmpProjectViewView from '../lib/emp-project-view-view';
+
+describe('EmpProjectViewView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
